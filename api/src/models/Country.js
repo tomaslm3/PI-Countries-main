b28c02674f0ac04fcfc3677225b75f73a7ddbdc1
Tom <tomaslm_3@hotmail.co>
2022-03-27 11:59:59 -0300

    Country model added

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -4,9 +4,38 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('country', {
+    id:{
+      type:DataTypes.STRING(3),
+      allowNull:false,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    flags: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    continents: {
+      type:DataTypes.STRING,
+      allowNull: false
+    },
+    capital: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    subregion: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    area: {
+      type: DataTypes.FLOAT,
+      allowNull:false
+    },
+    population: {
+      type: DataTypes.INTEGER,
+      allowNull:false
+    }
   });
 };
