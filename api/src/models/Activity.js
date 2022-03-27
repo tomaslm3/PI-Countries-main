87ca99144eb0bfbb6998939ecbfd5cc58ff1b779
Tom <tomaslm_3@hotmail.co>
2022-03-27 12:25:32 -0300

    Activity model added

---
+++ api/src/models/Activity.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+
+    sequelize.define('activity', {
+        name:{
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        difficulty: {
+            type: DataTypes.ENUM('1','2','3','4','5'),
+            allowNull: false
+        },
+        duration: {
+            type: DataTypes.DECIMAL,
+            allowNull:false
+        },
+        season: {
+            type: DataTypes.ENUM('Summer','Autumn','Winter','Spring'),
+            allowNull: false
+        },
+
+    });
+};
+
+// ID
+// Nombre
+// Dificultad (Entre 1 y 5)
+// Duración
+// Temporada (Verano, Otoño, Invierno o Primavera)
