e35a9e78b0752399a217c11cfba17978d12de4c1
Tom <tomaslm_3@hotmail.co>
2022-03-28 21:34:26 -0300

    Component Home

---
+++ client/src/components/home/HomeComponent.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function HomeComponent() {
+    return(
+        <div>
+            <h1>Countries</h1>
+            <div className="cards">
+                
+            </div>
+        </div>
+        
+    )
+}
+
+export default HomeComponent;
