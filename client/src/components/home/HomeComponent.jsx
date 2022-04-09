9bfb6e081af01138b0540588e1308a309efa4edb
Tom <tomaslm_3@hotmail.co>
2022-04-09 20:46:46 -0300

    Pagination done

--- client/src/components/home/HomeComponent.jsx
+++ client/src/components/home/HomeComponent.jsx
@@ -1,21 +1,33 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllCountries } from "../../redux/actions";
 import CardsComponent from "../cards/CardsComponent";
+import PaginationComponent from "../pagination/PaginationComponent";
 
 function HomeComponent() {
     const dispatch = useDispatch();
     const allCountries = useSelector(state => state.allCountries)
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPages] = useState(10);
 
     useEffect(() => {
         dispatch(getAllCountries())
     }, [dispatch])
 
+    const indexOfLastItem = currentPage * itemsPerPages;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPages;
+    const currentItems = allCountries.slice(indexOfFirstItem, indexOfLastItem)
+
+
     return(
         <div>
             <h1>Countries</h1>
             <div className="cards">
-                <CardsComponent allCountries={allCountries}/>
+                <CardsComponent allCountries={currentItems}/>
+
+                <div>
+                    <PaginationComponent allCountries={allCountries} itemsPerPages={itemsPerPages} setCurrentPage={setCurrentPage} currentPage={currentPage}/>
+                </div>
             </div>
         </div>
         
--- client/src/components/newActivity/validators.js
+++ client/src/components/newActivity/validators.js
@@ -4,16 +4,16 @@ const validate = (activity) => {
         errors.name = 'Nombre Requerido*'
     };
     if(!activity.difficulty){
-        errors.difficulty = 'Seleccionar un nivel de dificultad'
+        errors.difficulty = 'Seleccionar un nivel de dificultad*'
     };
     if(activity.duration > 24 || activity.duration < 1 ){
-        errors.duration = 'Duracion maxima de 1 a 24 hs'
+        errors.duration = 'Duracion maxima de 1 a 24 hs*'
     };
     if(!activity.season){
-        errors.season = 'Seleccionar una temporada'
+        errors.season = 'Seleccionar una temporada*'
     };
     if(!activity.countries.length){
-        errors.countries = 'Seleccionar por lo menos un pais'
+        errors.countries = 'Seleccionar por lo menos un pais*'
     };
 
     return errors;
